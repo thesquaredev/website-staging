a97c026fe1d34c3a66a15270aa37fdc651ff64f3
Kostas <kostas.siabanis@gmail.com>
2019-07-05 16:11:40 +0000

    auto commit [ci skip]

--- sw.js
+++ sw.js
@@ -29,14 +29,14 @@ self.__precacheManifest = [
     "url": "webpack-runtime-e2b23e0c861f424a40e9.js"
   },
   {
-    "url": "app-54749c5c8212d8d4d9d4.js"
+    "url": "app-560a7bec147423829bcb.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-b953aa0db9424edca55d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bc5e42de18d141ebf9d3a5cf90f0e070"
+    "revision": "1e7b34438e75ef03f2d43b3996040f77"
   },
   {
     "url": "1.e64ab458eefa347bc50c.css"
