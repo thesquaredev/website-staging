498f4ae04b6583e4adea1bed1dee693d5ec40223
Kostas Siabanis <kostas.siabanis@gmail.com>
2019-07-06 17:04:20 +0300

    Redirect not found pages to home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import Layout from '../components/layout/layout'
+import { Redirect } from '@reach/router'
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => <Redirect to="/" />
 
 export default NotFoundPage
