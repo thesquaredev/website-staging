7f9e43c5a8b45f312cbe369e0548341c59765ef3
Kostas Siabanis <ksiabani@users.noreply.github.com>
2019-09-04 11:57:58 +0300

    Fix inserting html from markdown (#42)

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -3,20 +3,31 @@ import './grid.scss'
 import withLocation from '../common/withLocation'
 import withShowcase from '../common/withShowcase/withShowcase'
 
-// Subcomponents
+/**
+ * This component renders a tile
+ * @param heading The main title of the tile
+ * @param icon The icon to show on the left of the tile
+ * @param description The text of the tile
+ */
 const Tile = ({ heading, icon, description }) => (
   <div className="single-offer d-flex flex-row">
     <div className="icon">
       <img src={`/icons/${icon}`} alt="" />
     </div>
     <div className="desc">
-      {heading && <h3>{heading}</h3>}
+      {heading && <h3 dangerouslySetInnerHTML={{ __html: heading }} />}
       <div dangerouslySetInnerHTML={{ __html: description }} />
     </div>
   </div>
 )
 
-// Component
+/**
+ * This component renders a grid with a number of tiles.
+ * @param heading The main title of the component
+ * @param description The subtitle of the component
+ * @param tiles The tiles to show
+ * @param elemId The id of the root element of the component
+ */
 const Grid = ({ meta: { heading, description, tiles }, elemId }) => (
   <section className="grid section-gap" id={elemId}>
     <div className="grid__content container">
@@ -24,7 +35,7 @@ const Grid = ({ meta: { heading, description, tiles }, elemId }) => (
         <div className="menu-content pb-60 col-lg-10">
           <div className="title text-center">
             <h1 className="mb-10">{heading}</h1>
-            <p>{description}</p>
+            {description && <p>{description}</p>}
           </div>
         </div>
       </div>
