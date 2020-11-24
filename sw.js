aa237b99ee5502378ed7ed641154031fe5511db3
Kostas <kostas.siabanis@gmail.com>
2020-11-24 10:39:09 +0000

    auto commit [ci skip]

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0cd284bdb5ed9cb3e038.js"
+    "url": "webpack-runtime-c5d3231bde56b5668602.js"
   },
   {
     "url": "app-7e81bb9a106f806706ce.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "044c4f873fe43951daa651d82535fa6c"
+    "revision": "e7e007a60b2b96f6b15988ceccff194f"
   },
   {
     "url": "manifest.webmanifest",
